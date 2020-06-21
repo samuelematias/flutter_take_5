49bf30df8d742b31325c7a041ebfe7918f14f2e7
samuelematias <hi@samuelematias.com>
2020-06-21 18:56:46 -0300

    create ContactsScreen

---
+++ lib/ui/contacts/screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ContactsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts Screen'),
+      ),
+      body: Container(),
+    );
+  }
+}
