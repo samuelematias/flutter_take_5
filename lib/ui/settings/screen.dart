f1fb616ad397f777f950ffaf0c0575f2469299d2
samuelematias <hi@samuelematias.com>
2020-06-21 21:46:09 -0300

    add switch to dark mode on SettingsScreen

--- lib/ui/settings/screen.dart
+++ lib/ui/settings/screen.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_take_5/src/classes/settings.dart';
+import 'package:provider/provider.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _settings = Provider.of<Settings>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('Settings Screen'),
       ),
-      body: Container(),
+      body: ListView(
+        children: [
+          SwitchListTile(
+            title: Text('Dark mode'),
+            value: _settings.isDark,
+            onChanged: _settings.updateIsDark,
+          )
+        ],
+      ),
     );
   }
 }
